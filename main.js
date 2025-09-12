8dfd97b90ebe667e94187e2a9aa05be2e4aeeca2
arvin-bit <120641674+arvin-bit@users.noreply.github.com>
2025-09-12 22:12:12 +0200

    fixed version

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import kaplay from "https://unpkg.com/kaplay@3001.0.19/dist/kaplay.mjs";
+import kaplay from "https://unpkg.com/kaplay/dist/kaplay.mjs";
 
 kaplay({
     background: [141, 183, 255],
