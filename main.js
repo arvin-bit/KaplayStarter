66275b1389c2ebd5bd12cc5c8edadbc3af862b6f
arvin-bit <120641674+arvin-bit@users.noreply.github.com>
2025-10-01 11:07:34 +0200

    TRYDIS

--- main.js
+++ main.js
@@ -1,4 +1,38 @@
 import kaplay from "https://unpkg.com/kaplay@3001.0.19/dist/kaplay.mjs";
 
-kaplay();
+kaplay({
+  background: "darkSalmon",
+  debug: true,
+});
 
+
+let score = 0;
+
+const scoreText = add([
+  text("Score: 0", { size: 32 }),
+  pos(20, 20),
+  z(100),
+  {
+    update() {
+      this.text = "Score: " + score;
+    },
+  },
+]);
+
+const bean = add([
+  sprite("bean"),
+  pos(center()),
+  area(),
+  scale(2),
+  anchor("center"),
+]);
+
+bean.onClick(() => {
+  score++;
+});
+
+bean.onClick(() => {
+  score++;
+  shake(5);
+  flash("#cc425e", 0.2);
+});
